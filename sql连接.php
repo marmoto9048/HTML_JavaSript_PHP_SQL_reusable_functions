d9b69f48d52ef03122606b7a47a2eb8db8b55537
noober_cypok <34801729+marmoto9048@users.noreply.github.com>
2022-04-04 18:19:40 +0900

    Create sql连接.php

---
+++ sql连接.php
@@ -0,0 +1,22 @@
+<?php
+$dbServer ="xxxxx"; //服务器IP地址,如果是本地，可以写成localhost
+$dbUser ="xxx"; //用户名
+$dbPassword ="xxxxx"; //密码
+$dbName ="xxxxxx"; //数据库名称
+
+$connectionInfo = array(
+    "Database" => $dbName,
+    "UID" => $dbUser,
+    "PWD" => $dbPassword
+);
+$conn = sqlsrv_connect($dbServer, $connectionInfo);
+////执行查询语句
+$query ="SELECT *  FROM S4_棚卸結果貼付 ";
+$query = iconv('UTF-8','cp932',$query);
+$stmt = sqlsrv_query($conn, $query);
+if( $stmt === false) {
+    die( print_r( sqlsrv_errors(), true) );
+}
+while($row = sqlsrv_fetch_array($stmt))
+{echo $row[0]."-----".$row[1]."\n";}
+?>
